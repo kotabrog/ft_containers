0cf071542877696ec2eceec1137f397f1ebc3e72
kotabrog <suzuki.kota314@gmail.com>
2022-06-05 20:59:02 +0900

    add ft before lexicographical_compare

--- inc/map.hpp
+++ inc/map.hpp
@@ -296,7 +296,7 @@ template<class Key, class T, class Compare, class Alloc>
 bool operator<(const map<Key, T, Compare, Alloc>& lhs,
                 const map<Key, T, Compare, Alloc>& rhs)
 {
-    return lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+    return ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
 }
 
 template<class Key, class T, class Compare, class Alloc>
